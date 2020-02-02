3f4122dc607025fbef593a3b521fb5b7789914a3
minhan <alsgks1218@gmail.com>
2020-02-02 13:47:00 +0900

    smiley cam

--- App.js
+++ App.js
@@ -1,19 +1,165 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  ActivityIndicator,
+  Dimensions,
+  TouchableOpacity
+} from "react-native";
+import { Camera } from "expo-camera";
+import * as Permissions from "expo-permissions";
+import styled from "styled-components/native";
+import { MaterialIcons } from "@expo/vector-icons";
+import * as FaceDetector from "expo-face-detector";
+import * as MediaLibrary from "expo-media-library";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const { width, height } = Dimensions.get("window");
+
+const ALBUM_NAME = "Smiley Cam";
+
+const CenterView = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: cornflowerblue;
+`;
+
+const Text = styled.Text`
+  color: white;
+  font-size: 22px;
+`;
+
+const IconBar = styled.View`
+  margin-top: 50px;
+`;
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasPermission: null,
+      cameraType: Camera.Constants.Type.front,
+      smileDetected: false
+    };
+    this.cameraRef = React.createRef();
+  }
+
+  componentDidMount = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    console.log(status);
+    if (status === "granted") {
+      this.setState({
+        hasPermission: true
+      });
+    } else {
+      this.setState({
+        hasPermission: false
+      });
+    }
+  };
+  render() {
+    const { hasPermission, cameraType, smileDetected } = this.state;
+    if (hasPermission === true) {
+      return (
+        <CenterView>
+          <Camera
+            style={{
+              width: width - 40,
+              height: height / 1.5,
+              borderRadius: 10,
+              overflow: "hidden"
+            }}
+            type={cameraType}
+            onFacesDetected={smileDetected ? null : this.onFaceDetected}
+            faceDetectorSettings={{
+              detectLandmarks: FaceDetector.Constants.Landmarks.all,
+              runClassifications: FaceDetector.Constants.Classifications.all
+            }}
+            ref={this.cameraRef}
+          />
+          <IconBar>
+            <TouchableOpacity onPress={this.swichCameraType}>
+              <MaterialIcons
+                name={
+                  cameraType === Camera.Constants.Type.front
+                    ? "camera-rear"
+                    : "camera-front"
+                }
+                color="white"
+                size={50}
+              />
+            </TouchableOpacity>
+          </IconBar>
+        </CenterView>
+      );
+    } else if (hasPermission === false) {
+      return (
+        <CenterView>
+          <Text>No Permission</Text>
+        </CenterView>
+      );
+    } else {
+      return (
+        <CenterView>
+          <ActivityIndicator />
+        </CenterView>
+      );
+    }
+  }
+  swichCameraType = () => {
+    const { cameraType } = this.state;
+    if (cameraType === Camera.Constants.Type.front) {
+      this.setState({
+        cameraType: Camera.Constants.Type.back
+      });
+    } else {
+      this.setState({
+        cameraType: Camera.Constants.Type.front
+      });
+    }
+  };
+  onFaceDetected = ({ faces }) => {
+    const face = faces[0];
+    if (face) {
+      if (face.smilingProbability > 0.7) {
+        this.setState({
+          smileDetected: true
+        });
+        this.takePhoto();
+      }
+    }
+  };
+  takePhoto = async () => {
+    try {
+      if (this.cameraRef.current) {
+        let { uri } = await this.cameraRef.current.takePictureAsync({
+          quality: 1
+        });
+        this.savePhoto(uri);
+      }
+    } catch (error) {
+      alert(error);
+      this.setState({
+        smileDetected: false
+      });
+    }
+  };
+
+  savePhoto = async uri => {
+    try {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status === "granted") {
+        const assets = await MediaLibrary.createAssetAsync(uri);
+        let album = await MediaLibrary.getAlbumAsync(ALBUM_NAME);
+        if (album === null) {
+          album = await MediaLibrary.createAlbumAsync(ALBUM_NAME, assets);
+        } else {
+          await MediaLibrary.addAssetsToAlbumAsync([assets], album.id);
+        }
+        setTimeout(() => this.setState({ smileDetected: false }), 1500);
+      } else {
+        this.setState({ hasPermission: false });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 android/ios app that take a picture when you smiling.
 노마드코더를 보고 따라 만든 앱입니다.
 
+## 완성 모습
+
+![smiley-cam](https://drive.google.com/uc?id=1KkCnwsQzowvUH-jSTrvCSOYV9zBO6FlX)  
+버튼을 누르면 카메라가 전후방 교체 가능  
+
+## expo 권한 허용
+
+```js
+import * as Permissions from "expo-permissions";
+
+const { status } = await Permissions.askAsync(권한);
+// 권한은 "granted"가 되면 승인된 것임
+```
+
+각 기능마다 권한이 허용되면 사용 가능함 
